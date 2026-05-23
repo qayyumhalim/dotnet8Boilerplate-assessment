d37651bba50be86f527cf71b6645cedcddabaf14
Qayyum <abdul.qayyum@uis.ai>
2026-05-23 14:22:52 +0800

    Task 4 - create unit tests for customer

---
+++ src/AcmePortal/Common/CustomerRules.cs
@@ -0,0 +1,15 @@
+﻿using AcmePortal.Model;
+
+namespace AcmePortal.Common
+{
+    public static class CustomerRules
+    {
+        public static (bool IsValid, string? ErrorMessage) ValidateDeactivation(Customer customer, int openOrderCount)
+        {
+            if (openOrderCount > 0)
+                return (false, $"Cannot deactivate customer with {openOrderCount} open order(s).");
+
+            return (true, null);
+        }
+    }
+}
---
+++ tests/AcmePortal.Tests/Workflow/CustomerRulesTests.cs
@@ -0,0 +1,31 @@
+﻿using AcmePortal.Model;
+using AcmePortal.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmePortal.Tests.Workflow
+{
+    public class CustomerRulesTests
+    {
+        [Fact]
+        public void ValidateDeactivation_NoOpenOrders_IsValid()
+        {
+            var customer = new Customer { Id = 1, Name = "Acme" };
+            var (isValid, error) = CustomerRules.ValidateDeactivation(customer, 0);
+            Assert.True(isValid);
+            Assert.Null(error);
+        }
+
+        [Fact]
+        public void ValidateDeactivation_HasOpenOrders_IsInvalid()
+        {
+            var customer = new Customer { Id = 1, Name = "Acme" };
+            var (isValid, error) = CustomerRules.ValidateDeactivation(customer, 3);
+            Assert.False(isValid);
+            Assert.NotNull(error);
+        }
+    }
+}
